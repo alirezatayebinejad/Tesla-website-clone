ab09cd796d42297f334e227731cf7a277f061f8c
vibracode <codevibra@gmail.com>
2022-08-22 19:34:33 +0430

    section component done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from "react";
-import logo from "./logo.svg";
-import { Counter } from "./features/counter/Counter";
 import "./App.css";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Section from "./components/Section";
 
 function App() {
-	return <div className="App"></div>;
+	return (
+		<div className="App">
+			<Section title="Model 3" description="Order Online for Touchless Delivery" btnFirst="custom order" btnSecond="existing inventory" imgSrc="model-3.jpg" />
+			<Section title="Model Y" description="Order Online for Touchless Delivery" btnFirst="custom order" btnSecond="existing inventory" imgSrc="model-y.jpg" />
+			<Section title="Model S" description="Order Online for Touchless Delivery" btnFirst="custom order" btnSecond="existing inventory" imgSrc="model-s.jpg" />
+			<Section title="Model X" description="Order Online for Touchless Delivery" btnFirst="custom order" btnSecond="existing inventory" imgSrc="model-x.jpg" />
+			<Section title="Solar Panels" description="Lowest Cost Solar Panels in America" btnFirst="Order Now" btnSecond="Learn more" imgSrc="solar-panel.jpg" />
+			<Section title="Solar Roof" description="Produce Clean Energy From Your Roof" btnFirst="Order Now" btnSecond="Learn more" imgSrc="solar-roof.jpg" />
+			<Section title="Accessories" description="" btnFirst="shop now" btnSecond="" imgSrc="accessories.jpg" />
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/Section.js
@@ -0,0 +1,86 @@
+import React from "react";
+import styled from "styled-components";
+
+function Section({ title, description, btnFirst, btnSecond, imgSrc }) {
+	return (
+		<Wrap bgImage={imgSrc}>
+			<HeadTitle>
+				<Title>{title}</Title>
+				<Description>{description}</Description>
+			</HeadTitle>
+			<DownSec>
+				<Bottons>
+					<BtnFirst>{btnFirst}</BtnFirst>
+					{btnSecond && <BtnSecond>{btnSecond}</BtnSecond>}
+				</Bottons>
+				<DownArrow src="/images/down-arrow.svg"></DownArrow>
+			</DownSec>
+		</Wrap>
+	);
+}
+
+export default Section;
+
+const Wrap = styled.div`
+	width: 100vw;
+	height: 100vh;
+	background-image: ${(props) => `url("/images/${props.bgImage}")`};
+	background-size: cover;
+	background-repeat: no-repeat;
+	background-position: center;
+	display: flex;
+	flex-direction: column;
+	justify-content: space-between;
+	align-items: center;
+`;
+const HeadTitle = styled.div`
+	margin-top: 80px;
+	text-align: center;
+`;
+const Title = styled.h1`
+	color: #171a20;
+	font-size: 45px;
+`;
+const Description = styled.p`
+	color: #57595d;
+	margin-top: 8px;
+`;
+const Bottons = styled.div`
+	display: flex;
+	@media (max-width: 600px) {
+		flex-direction: column;
+	}
+`;
+const DownSec = styled.div`
+	display: flex;
+	align-items: center;
+	flex-direction: column;
+`;
+const BtnFirst = styled.button`
+	font-size: 12px;
+	text-transform: uppercase;
+	width: 260px;
+	height: 40px;
+	border-radius: 100px;
+	border: none;
+	background-color: RGB(61, 64, 68);
+	opacity: 0.85;
+	color: white;
+	cursor: pointer;
+`;
+const BtnSecond = styled(BtnFirst)`
+	background-color: white;
+	opacity: 0.55;
+	color: black;
+	margin-left: 25px;
+	@media (max-width: 600px) {
+		margin-left: 0px;
+		margin-top: 15px;
+	}
+`;
+const DownArrow = styled.img`
+	width: 35px;
+	margin: 50px 0 20px;
+	cursor: pointer;
+	animation: animateDown infinite 1.5s;
+`;

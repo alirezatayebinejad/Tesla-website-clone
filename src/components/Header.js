7bd34c6b02328daf4ff1163ea5e240b943031b90
vibracode <codevibra@gmail.com>
2022-08-23 10:48:57 +0430

    header done

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,78 @@
 import React from "react";
+import styled from "styled-components";
 
 function Header() {
-	return <div>Header</div>;
+	return (
+		<Container>
+			<Logo>
+				<a href="#">
+					<img src="/images/logo.svg" alt="tesla logo" />
+				</a>
+			</Logo>
+			<MenuMid>
+				<a href="#">Model 3</a>
+				<a href="#">Model y</a>
+				<a href="#">Model s</a>
+				<a href="#">Model x</a>
+				<a href="#">Solar Roof</a>
+				<a href="#">Solar Panels</a>
+			</MenuMid>
+			<MenuRight>
+				<a href="#">Shop</a>
+				<a href="#">Account</a>
+				<a href="#">Menu</a>
+			</MenuRight>
+		</Container>
+	);
 }
 
 export default Header;
+
+const Container = styled.div`
+	display: flex;
+	flex-wrap: nowrap;
+	align-items: center;
+	justify-content: space-between;
+	height: 55px;
+	width: 100%;
+	position: fixed;
+	padding: 0 40px;
+`;
+const Logo = styled.div``;
+const MenuMid = styled.div`
+	margin-right:100px
+	height: 100%;
+	display: flex;
+	align-items: center;
+	margin-right:-100px;
+	a {
+		color: black;
+		font-weight: 600;
+		padding: 10px 10px;
+		color: #3b3b3b;
+	}
+	a:hover {
+		background-color: rgba(66, 66, 66, 0.092);
+		border-radius: 13px;
+		transition: background-color 0.4s;
+	}
+	@media (max-width: 1000px) {
+		display: none;
+	} ;
+`;
+const MenuRight = styled.div`
+	height: 100%;
+	display: flex;
+	align-items: center;
+	a {
+		color: black;
+		font-weight: bold;
+		padding: 10px 10px;
+		color: #3b3b3b;
+	}
+	a:hover {
+		background-color: rgba(66, 66, 66, 0.092);
+		border-radius: 13px;
+		transition: background-color 0.4s;
+	}
+`;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,18 @@
 import React from "react";
+import Section from "./Section";
 
 function Home() {
-	return <div>Home</div>;
+	return (
+		<div>
+			<Section title="Model 3" description="Order Online for Touchless Delivery" btnFirst="custom order" btnSecond="existing inventory" imgSrc="model-3.jpg" />
+			<Section title="Model Y" description="Order Online for Touchless Delivery" btnFirst="custom order" btnSecond="existing inventory" imgSrc="model-y.jpg" />
+			<Section title="Model S" description="Order Online for Touchless Delivery" btnFirst="custom order" btnSecond="existing inventory" imgSrc="model-s.jpg" />
+			<Section title="Model X" description="Order Online for Touchless Delivery" btnFirst="custom order" btnSecond="existing inventory" imgSrc="model-x.jpg" />
+			<Section title="Solar Panels" description="Lowest Cost Solar Panels in America" btnFirst="Order Now" btnSecond="Learn more" imgSrc="solar-panel.jpg" />
+			<Section title="Solar Roof" description="Produce Clean Energy From Your Roof" btnFirst="Order Now" btnSecond="Learn more" imgSrc="solar-roof.jpg" />
+			<Section title="Accessories" description="" btnFirst="shop now" btnSecond="" imgSrc="accessories.jpg" />
+		</div>
+	);
 }
 
 export default Home;
